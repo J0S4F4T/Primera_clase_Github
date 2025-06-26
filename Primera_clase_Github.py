7f6c82da68e874b159406408e54431b16825ed89
J0S4F4T <jos.mencia@duocuc.cl>
2025-06-26 14:19:54 -0400

    Función resta

--- Primera_clase_Github.py
+++ Primera_clase_Github.py
@@ -1,5 +1,11 @@
 def sumar(a,b):
     return a+b
+
+def resta(a,b):
+    return a-b
+
 num1=int(input("Ingrese valor: "))
 num2=int(input("Ingrese valor: "))
-print(sumar(num1,num2))
+
+print("La suma es",sumar(num1,num2))
+print("La resta es",resta(num1,num2))
